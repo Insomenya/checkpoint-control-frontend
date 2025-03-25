7ca10ffa5404bd0ca9500e902907924eb317051a
Insomenya <kolowertnov.alexej@yandex.com>
2025-03-26 00:04:18 +0400

    fixed refresh jwt bug

--- src/app/Verify/Verify.tsx
+++ src/app/Verify/Verify.tsx
@@ -1,15 +1,18 @@
 import { useVerifyTokenMutation } from "@api/auth/authApi";
 import { selectToken } from "@store/auth/auth.selectors";
 import { useAppSelector } from "@store/store";
+import { useEffect } from "react";
 
 
 export const Verify = () => {
     const token = useAppSelector(selectToken);
     const [verifyToken] = useVerifyTokenMutation();
 
-    if (token) {
-        verifyToken({ token });
-    }
+    useEffect(() => {
+        if (token) {
+            verifyToken({ token });
+        }
+    }, []);
 
     return null;
 };
